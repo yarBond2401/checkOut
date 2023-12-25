1cd1d75d832d297acae57ae6a7c2cd4efe34b525
Nikita <niknechaev345@gmail.com>
2023-12-25 23:09:44 +0200

    first commit

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import styles from './Footer.module.scss';
+import Link from 'next/link';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.links}>
+          <Link href={'/'}>
+            <img src="/assets/footer/01.png" alt="Company Logotype" />
+          </Link>
+          <Link href={'/'}>
+            <img src="/assets/footer/02.png" alt="Company Logotype" />
+          </Link>
+          <Link href={'/'}>
+            <img src="/assets/footer/03.png" alt="Company Logotype" />
+          </Link>
+          <Link href={'/'}>
+            <img src="/assets/footer/04.png" alt="Company Logotype" />
+          </Link>
+        </div>
+        <div className={styles.reference}>
+          Powered by <span className={styles.reference_gray}>Stripe | Terms Privacy</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
