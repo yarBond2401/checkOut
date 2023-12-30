017fdc2e6700bee260edf0e1d995f5bac9d8b56b
Nikita <niknechaev345@gmail.com>
2023-12-30 14:51:33 +0200

    server

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { RiErrorWarningFill } from 'react-icons/ri';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import { OnApproveData, CreateOrderData } from '@paypal/paypal-js/types';
 import styles from '../styles/main.module.scss';
 import React, { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
@@ -25,6 +26,7 @@ const HomePage: React.FC = () => {
   const [isPopup, setPopup] = useState<boolean>(false);
   const [isShowSnackbar, setShowSnackbar] = useState<boolean>(false);
   const [submitClick, setSubmitClick] = useState<number>(0);
+  const [price, setPrice] = useState<number>(20.000)
 
   const {
     register,
@@ -34,14 +36,6 @@ const HomePage: React.FC = () => {
     setValue,
     setError,
   } = useForm<IForm>({ shouldFocusError: false });
-
-  const onSubmit: SubmitHandler<IForm> = async (data) => {
-    if (!isAgree) return setShowSnackbar(true);
-    if (data.confirmEmail !== data.email) return setError('confirmEmail', { message: 'Email Adress Is Not Matched' });
-
-    setPopup(true);
-    console.log('data', data);
-  };
 
   useEffect(() => {
     if (isShowSnackbar || Object.keys(errors).length > 0) {
@@ -62,78 +56,135 @@ const HomePage: React.FC = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+  /* ============================================================================================================================= */
+
+  const serverUrl = 'http://localhost:8888/';
+
+  const initialOptions = {
+    clientId: 'ClientIdFromUser',
+    currency: 'USD',
+    intent: 'capture',
+  };
+
+  const createOrder = async (data: CreateOrderData) => {
+    // Order is created on the server and the order id is returned
+    const response = await fetch(`${serverUrl}/my-server/create-paypal-order`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // используйте параметр «body», чтобы при необходимости передать дополнительную информацию о заказе
+      // например, артикулы и количествo продуктов
+      body: JSON.stringify({
+        product: {
+          description: 'THE_BEST_PRODUCT_NAME',
+          cost: price,
+        },
+      }),
+    });
+    const order = await response.json();
+    return order.id;
+  };
+  const onApprove = async (data: OnApproveData) => {
+    // Order is captured on the server and the response is returned to the browser
+    const response = await fetch(`${serverUrl}/my-server/capture-paypal-order`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        orderID: data.orderID,
+      }),
+    });
+    return await response.json();
+  };
+
+  const onSubmit: SubmitHandler<IForm> = async (data) => {
+    if (!isAgree) return setShowSnackbar(true);
+    if (data.confirmEmail !== data.email) return setError('confirmEmail', { message: 'Email Adress Is Not Matched' });
+
+    if (paymentMethod === PaymentMethodEnum.PAYPAL) {
+      console.log('data', data);
+    } else if (paymentMethod === PaymentMethodEnum.CREDIT_CARD) {
+      setPopup(true);
+      console.log('data', data);
+    }
+  };
 
   return (
-    <section id="section" className={styles.section}>
-      {isPopup && <Popup setPopup={setPopup} />}
-      <div className={clsx(styles.container, { [styles.container__error]: Object.keys(errors).length > 0 || isShowSnackbar })}>
-        {(isShowSnackbar || Object.keys(errors).length > 0) && (
-          <Snackbar submitClick={submitClick} isAgree={isAgree} paymentMethod={paymentMethod} control={control} errors={errors} />
-        )}
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-          {windowWidth > 768 && (
-            <>
-              <Payment
-                setValue={setValue}
-                isAgree={isAgree}
-                isSubscribe={isSubscribe}
-                setSubscribe={setSubscribe}
-                setAgree={setAgree}
-                control={control}
-                register={register}
-                paymentMethod={paymentMethod}
-                setPaymentMethod={setPaymentMethod}
-              />
-              <div className={styles.summary}>
-                <div className={styles.summary__content}>
-                  <h3 className={styles.summary__title}>Order Summary</h3>
-                  <div className={styles.divider}></div>
-                  <PricingPlan pricingPlan={pricingPlan} setPricingPlan={setPricingPlan} hours={hours} setHours={setHours} />
-                  <OrderSummary
-                    isAgree={isAgree}
-                    setShowSnackbar={setShowSnackbar}
-                    setSubmitClick={setSubmitClick}
-                    setDiscountCode={setDiscountCode}
-                    discountCode={discountCode}
-                  />
-                </div>
-              </div>
-            </>
+    <PayPalScriptProvider options={initialOptions}>
+      {/* <section id="section" className={styles.section}>
+        {isPopup && <Popup setPopup={setPopup} />}
+        <div className={clsx(styles.container, { [styles.container__error]: Object.keys(errors).length > 0 || isShowSnackbar })}>
+          {(isShowSnackbar || Object.keys(errors).length > 0) && (
+            <Snackbar submitClick={submitClick} isAgree={isAgree} paymentMethod={paymentMethod} control={control} errors={errors} />
           )}
-          {windowWidth <= 768 && (
-            <>
-              <div className={styles.summary}>
-                <div className={styles.summary__content}>
-                  <h3 className={styles.summary__title}>Pricing Plan</h3>
-                  <div className={styles.divider}></div>
-                  <PricingPlan pricingPlan={pricingPlan} setPricingPlan={setPricingPlan} hours={hours} setHours={setHours} />
-                  <Payment
-                    setValue={setValue}
-                    isAgree={isAgree}
-                    isSubscribe={isSubscribe}
-                    setSubscribe={setSubscribe}
-                    setAgree={setAgree}
-                    control={control}
-                    register={register}
-                    paymentMethod={paymentMethod}
-                    setPaymentMethod={setPaymentMethod}
-                  />
-                  <h3 className={styles.summary__title}>Order summary</h3>
-                  <div className={styles.divider}></div>
-                  <OrderSummary
-                    isAgree={isAgree}
-                    setShowSnackbar={setShowSnackbar}
-                    setSubmitClick={setSubmitClick}
-                    setDiscountCode={setDiscountCode}
-                    discountCode={discountCode}
-                  />
+          <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            {windowWidth > 768 && (
+              <>
+                <Payment
+                  setValue={setValue}
+                  isAgree={isAgree}
+                  isSubscribe={isSubscribe}
+                  setSubscribe={setSubscribe}
+                  setAgree={setAgree}
+                  control={control}
+                  register={register}
+                  paymentMethod={paymentMethod}
+                  setPaymentMethod={setPaymentMethod}
+                />
+                <div className={styles.summary}>
+                  <div className={styles.summary__content}>
+                    <h3 className={styles.summary__title}>Order Summary</h3>
+                    <div className={styles.divider}></div>
+                    <PricingPlan pricingPlan={pricingPlan} setPricingPlan={setPricingPlan} hours={hours} setHours={setHours} />
+                    <OrderSummary
+                      isAgree={isAgree}
+                      setShowSnackbar={setShowSnackbar}
+                      setSubmitClick={setSubmitClick}
+                      setDiscountCode={setDiscountCode}
+                      discountCode={discountCode}
+                    />
+                  </div>
                 </div>
-              </div>
-            </>
-          )}
-        </form>
-      </div>
-    </section>
+              </>
+            )}
+            {windowWidth <= 768 && (
+              <>
+                <div className={styles.summary}>
+                  <div className={styles.summary__content}>
+                    <h3 className={styles.summary__title}>Pricing Plan</h3>
+                    <div className={styles.divider}></div>
+                    <PricingPlan pricingPlan={pricingPlan} setPricingPlan={setPricingPlan} hours={hours} setHours={setHours} />
+                    <Payment
+                      setValue={setValue}
+                      isAgree={isAgree}
+                      isSubscribe={isSubscribe}
+                      setSubscribe={setSubscribe}
+                      setAgree={setAgree}
+                      control={control}
+                      register={register}
+                      paymentMethod={paymentMethod}
+                      setPaymentMethod={setPaymentMethod}
+                    />
+                    <h3 className={styles.summary__title}>Order summary</h3>
+                    <div className={styles.divider}></div>
+                    <OrderSummary
+                      isAgree={isAgree}
+                      setShowSnackbar={setShowSnackbar}
+                      setSubmitClick={setSubmitClick}
+                      setDiscountCode={setDiscountCode}
+                      discountCode={discountCode}
+                    />
+                  </div>
+                </div>
+              </>
+            )}
+          </form>
+        </div>
+      </section> */}
+      <PayPalButtons createOrder={(data, actions) => createOrder(data)} onApprove={(data, actions) => onApprove(data)} />
+    </PayPalScriptProvider>
   );
 };
 export default HomePage;
